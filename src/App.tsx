e642e321830c0b5e64da20fc4be40bde709b2c72
Josmi <jmlunguinho@gmail.com>
2021-10-01 12:12:36 -0300

    :tada: Chapter II ignite

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Dashboard } from "./components/Dashboard";
+import { useState } from 'react'
+import { Header } from "./components/Header";
+import { TransactionsProvider } from "./hooks/useTransactions";
+import { NewTransactionModal } from "./components/NewTransactionModal"
+import { GlobalStyle } from "./styles/global";
 
-function App() {
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleNewTransactionOpen(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleNewTransactionClose(){
+      setIsNewTransactionModalOpen(false);
+    }  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleNewTransactionOpen}/>
+      <Dashboard/>
+      <NewTransactionModal onOpenNewTransactionModal={isNewTransactionModalOpen} onCloseNewTransactionModal={handleNewTransactionClose}/>
+      <GlobalStyle/>
+    </TransactionsProvider>
   );
 }
 
-export default App;
+
---
+++ src/hooks/useTransactions.tsx
@@ -0,0 +1,64 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+interface Transaction {
+    id: number,
+    title: string,
+    amount: number,
+    type: string,
+    category: string,
+    createdAt: string
+}
+/*interface TransactionInput{
+    title: string,
+    amount: number,
+    type: string,
+    category: string,
+}*/
+ //ou
+//type TransactionInput = Omit<Transaction,'id'|'createAt'>
+//ou
+type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type'| 'category' >
+
+interface TransactionProviderProps {
+    children: ReactNode;
+}
+
+interface TransactionContextData {
+    transactions: Transaction[],
+    createTransaction: (transaction:TransactionInput)=>Promise<void>
+}
+
+const TransactonsContext = createContext<TransactionContextData>({} as TransactionContextData);
+
+export function TransactionsProvider({ children }:TransactionProviderProps){
+    const [transactions, setTransactions ] = useState<Transaction[]>([]);
+    useEffect(()=>{
+        api.get('transactions')
+        .then(response => setTransactions(response.data.transactions));
+    },[]);
+
+    async function createTransaction(transactionInput:TransactionInput){
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        })        
+        const { transaction } = response.data;
+
+        setTransactions([
+            ...transactions,
+            transaction,
+        ]);
+    }
+
+    return (
+        <TransactonsContext.Provider value={{transactions, createTransaction}}>
+            {children}
+        </TransactonsContext.Provider>
+    )
+}
+
+export function useTransactions(){
+    const context = useContext(TransactonsContext);
+    return context;
+}
